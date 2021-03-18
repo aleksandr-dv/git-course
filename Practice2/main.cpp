eb9b73234bc495f5741e1121379ed8da0c695e28
Sasha <aleksandr20031602@gmail.com>
2021-03-18 14:05:04 +0200

    first commit

---
+++ Practice2/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+/*
+		наслідування
+*/
+
+class Human
+{
+private:
+	string name;
+};
+
+class Student : public Human
+{
+public:
+	string group;
+	void learn()
+	{
+		cout << "I learn!" << endl;
+	}
+};
+
+class ExtramuralStudent : public Student
+{
+public:
+	void learn()
+	{
+		cout << "I also learn but from home!" << endl;
+	}
+};
+
+class Professor : public Human
+{
+public:
+	string subject;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "ukr");
+	cout << "Hello!" << endl;
+	return 0;
+}
